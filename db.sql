5586ec1f9b0c5d6dd729563184b21d0077ecb818
kobeliuke <kobeliuke@163.com>
2017-11-09 23:53:51 +0800

    update entity

--- db.sql
+++ db.sql
@@ -18,6 +18,8 @@ DROP TABLE IF EXISTS `tb_user_info`;
 --
 -- table tb_user_info
 --
+DROP TABLE  IF EXISTS `tb_user_info`;
+
 CREATE TABLE `tb_user_info`(
   `user_id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) DEFAULT NULL,
@@ -30,3 +32,77 @@ CREATE TABLE `tb_user_info`(
   `last_edit_time` datetime DEFAULT NULL,
   PRIMARY KEY(`user_id`)
 )ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- table tb_wechat_auth
+--
+
+DROP TABLE IF EXISTS `tb_wechat_auth`;
+
+CREATE TABLE `tb_wechat_auth`(
+  `wechat_auth_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `open_id` varchar(80) NOT NULL DEFAULT '',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`wechat_auth_id`),
+  UNIQUE KEY `open_id` (`open_id`),
+  KEY `fk_wechatauth_profile` (`user_id`),
+  CONSTRAINT `fk_wechatauth_profile` FOREIGN KEY (`user_id`) REFERENCES `tb_user_info`(`user_id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- table tb_local_auth
+--
+
+DROP TABLE IF EXISTS `tb_local_auth`;
+
+CREATE TABLE `tb_local_auth`(
+  `local_auth_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `last_eidt_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`local_auth_id`),
+  UNIQUE KEY `uk_local_profile` (`username`),
+  KEY `fk_localauth_profile`(`user_id`),
+  CONSTRAINT `fk_localauth_profile` FOREIGN KEY (`user_id`) REFERENCES `tb_user_info`(`user_id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- table tb_head_line
+--
+
+DROP TABLE if EXISTS `tb_head_line`;
+
+CREATE TABLE `tb_head_line`(
+  `line_id` int(100) NOT NULL AUTO_INCREMENT,
+  `line_name` varchar(1000) DEFAULT NULL,
+  `line_link` varchar(1000) NOT NULL,
+  `line_img` varchar(1000) NOT NULL,
+  `priority` int(2) DEFAULT NULL,
+  `enable_status` int(2) NOT NULL DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  PRIMARY KEY(`line_id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- table tb_shop_category
+--
+
+DROP TABLE IF EXISTS `tb_shop_category`;
+
+CREATE TABLE `tb_shop_category`	(
+  `shop_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_category_name` varchar(100) NOT NULL DEFAULT '',
+  `shop_category_desc` varchar(1000) DEFAULT '',
+  `shop_category_img` varchar(2000) DEFAULT NULL,
+  `priority` int(2)  NOT NULL DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`shop_category_id`),
+  KEY `fk_shop_category_self` (`parent_id`),
+  CONSTRAINT `fk_shop_category_self` FOREIGN KEY(`parent_id`) REFERENCES `tb_shop_category`(`shop_category_id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 CHARSET=utf8;
---
+++ src/main/java/com/will/o2o/entity/WechatAuth.java
@@ -0,0 +1,45 @@
+package com.will.o2o.entity;
+
+import java.util.Date;
+
+public class WechatAuth {
+
+    //primary key
+    private Long wechatAuthId;
+    //identified by wechat
+    private String openId;
+    private Date createTime;
+    private UserInfo userInfo;
+
+    public Long getWechatAuthId() {
+        return wechatAuthId;
+    }
+
+    public void setWechatAuthId(Long wechatAuthId) {
+        this.wechatAuthId = wechatAuthId;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+}
