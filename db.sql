15d65c04efd02eda269ac1799e639533651ad80f
kobeliuke <kobeliuke@163.com>
2017-11-15 21:22:58 +0800

    upadate all entity

--- db.sql
+++ db.sql
@@ -106,3 +106,92 @@ CREATE TABLE `tb_shop_category`	(
   KEY `fk_shop_category_self` (`parent_id`),
   CONSTRAINT `fk_shop_category_self` FOREIGN KEY(`parent_id`) REFERENCES `tb_shop_category`(`shop_category_id`)
 )ENGINE=INNODB AUTO_INCREMENT=1 CHARSET=utf8;
+
+--
+-- table tb_shop
+--
+DROP TABLE IF EXISTS `tb_shop`;
+
+CREATE TABLE `tb_shop`(
+  `shop_id` int(10) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(10) NOT NULL COMMENT 'shop creator',
+  `area_id` int(5) DEFAULT NULL,
+  `shop_category_id` int(11) DEFAULT NULL,
+  `shop_name` varchar(256) NOT NULL,
+  `shop_desc` varchar(1024) DEFAULT NULL,
+  `shop_addr` varchar(200) DEFAULT NULL,
+  `phone`     varchar(128) DEFAULT NULL,
+  `shop_img`  varchar(1024) DEFAULT NULL,
+  `priority` int(3) DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  `enable_status` int(2) NOT NULL DEFAULT '0',
+  `advice` varchar(255) DEFAULT NULL,
+  PRIMARY KEY(`shop_id`),
+  KEY `fk_shop_area` (`area_id`),
+  KEY `fk_shop_profile` (`owner_id`),
+  KEY `fk_shop_shopcate` (`shop_category_id`),
+  CONSTRAINT `fk_shop_area`     FOREIGN KEY (`area_id`) REFERENCES `tb_area`(`area_id`),
+  CONSTRAINT `fk_shop_profile`  FOREIGN KEY (`owner_id`) REFERENCES `tb_user_info`(`user_id`),
+  CONSTRAINT `fk_shop_shopcate` FOREIGN KEY (`shop_category_id`) REFERENCES `tb_shop_category` (`shop_category_id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- table tb_product_category
+--
+
+DROP TABLE IF EXISTS `tb_product_category`;
+
+CREATE TABLE `tb_product_category`(
+  `product_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_category_name` varchar(100) NOT NULL,
+  `priority` int(2) DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  `shop_id` int(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`product_category_id`),
+  KEY `fk_procate_shop` (`shop_id`),
+  CONSTRAINT `fk_procate_shop` FOREIGN KEY(`shop_id`) REFERENCES `tb_shop`(`shop_id`)
+)ENGINE =INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+--
+-- table tb_product
+--
+DROP TABLE IF EXISTS `tb_product`;
+
+CREATE TABLE `tb_product` (
+  `product_id` int(100) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(100) NOT NULL,
+  `product_desc` varchar(2000) DEFAULT NULL,
+  `img_addr` varchar(2000) DEFAULT '',
+  `normal_price` varchar(100) DEFAULT NULL,
+  `promotion_price` varchar(100) DEFAULT NULL,
+  `priority` int(2) DEFAULT '0',
+  `create_time`  datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  `enable_satatu` int(2) NOT NULL DEFAULT '0',
+  `product_category_id` int(11) DEFAULT NULL,
+  `shop_id` int(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`product_id`),
+  KEY `fk_product_procate`  (`product_category_id`),
+  KEY `fk_product_shop`(`shop_id`),
+  CONSTRAINT `fk_product_procate` FOREIGN KEY (`product_category_id`) REFERENCES `tb_product_category`(`product_category_id`),
+  CONSTRAINT `fk_product_shop` FOREIGN KEY (`shop_id`) REFERENCES `tb_shop`(`shop_id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- table tb_product_img
+--
+DROP TABLE IF EXISTS `tb_product_img`;
+
+CREATE TABLE `tb_product_img` (
+  `product_img_id` int(20) NOT NULL AUTO_INCREMENT,
+  `img_addr` varchar(2000) NOT NULL,
+  `img_desc` varchar(2000) DEFAULT NULL,
+  `priority` int(2) DEFAULT '0',
+  `create_time`  datetime DEFAULT NULL,
+  `product_id` int(20) DEFAULT NULL,
+  PRIMARY KEY (`product_img_id`),
+  KEY `fk_proimg_product`  (`product_id`),
+  CONSTRAINT `fk_proimg_product` FOREIGN KEY (`product_id`) REFERENCES `tb_product`(`product_id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
